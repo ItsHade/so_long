f4306b3d2b1d22d09e7e01ab208de5528211a91b
ItsHade <malo.burnet@gmail.com>
2023-07-25 01:37:09 +0200

    not working

--- src/map_check.c
+++ src/map_check.c
@@ -1,11 +1,48 @@
 #include "../include/so_long.h"
 
-//position x and y of the cell and front represent wich side we will check next 0 - top, 1 - right, 2 - bottom, 3 - left
-typedef struct s_pos {
+// position x and y of the cell and front represent wich side we will check next 0 - top, 1 - right, 2 - bottom, 3 - left
+typedef struct s_pos
+{
     int pos_x;
     int pos_y;
     int front;
 } t_cell;
+
+void ft_putcell(t_cell cell)
+{
+    ft_putchar('[');
+    ft_putchar(cell.pos_x + 48);
+    ft_putstr("][");
+    ft_putchar(cell.pos_y + 48);
+    ft_putstr("] Front: ");
+    ft_putchar(cell.front + 48);
+    ft_putchar('\n');
+}
+// use printf for the colors
+void ft_putmap(char **map, int nbLines, int lineLength)
+{
+    int line;
+    int col;
+
+    line = 0;
+    // printf("\033[1;31m");
+    while (line < nbLines)
+    {
+        col = 0;
+        while (col < lineLength)
+        {
+            ft_putchar('[');
+            ft_putchar(map[line][col]);
+            // printf("[%c]", map[line][col]);
+            ft_putchar(']');
+            col++;
+        }
+        ft_putchar('\n');
+        // printf("\n");
+        line++;
+    }
+    // printf("\033[0;37m");
+}
 
 int ft_check_file_format(char *file)
 {
@@ -39,7 +76,7 @@ int ft_check_file_format(char *file)
     return (0);
 }
 
-//renvoie la longeur sans le /n
+// renvoie la longeur sans le /n
 int ft_get_line_length(char *file)
 {
     char buffer[1];
@@ -64,7 +101,7 @@ int ft_get_line_length(char *file)
     return (length);
 }
 
-//use (buffer[0] == '\n' && prevBuffer[0] != '\n') to not count empty lines at the end of the file
+// use (buffer[0] == '\n' && prevBuffer[0] != '\n') to not count empty lines at the end of the file
 int ft_get_number_of_lines(char *file, int lineLength)
 {
     char buffer[1];
@@ -81,7 +118,7 @@ int ft_get_number_of_lines(char *file, int lineLength)
     if (fd < 0)
         return (-1);
     if (read(fd, buffer, 1) == 0)
-        return (close(fd), 0); 
+        return (close(fd), 0);
     currentLength++;
     while (nbBytesRead != 0)
     {
@@ -135,7 +172,7 @@ int ft_check_for_outside_walls(char **map, int nbLines, int lineLength)
             ft_putstr("Probleme mur du gauche\n");
             return (-1);
         }
-        line ++;
+        line++;
     }
     line = 0;
     while (line < nbLines)
@@ -150,7 +187,7 @@ int ft_check_for_outside_walls(char **map, int nbLines, int lineLength)
     return (0);
 }
 
-//and returns the number of collectibles
+// and returns the number of collectibles
 int ft_check_map_requirement(char **map, int nbLines, int lineLength)
 {
     int line;
@@ -178,7 +215,7 @@ int ft_check_map_requirement(char **map, int nbLines, int lineLength)
                 return (-1);
             col++;
         }
-        line ++;
+        line++;
     }
     if (countPlayerStart != 1 || countExits != 1 || countCollectibles < 1)
         return (-1);
@@ -194,7 +231,7 @@ t_cell ft_get_start_pos(char **map, int nbLines, int lineLength)
     line = 0;
     pos.pos_x = -1;
     pos.pos_y = -1;
-    //means we will got to the top first
+    // means we will got to the top first
     pos.front = 0;
     while (line < nbLines)
     {
@@ -211,7 +248,7 @@ t_cell ft_get_start_pos(char **map, int nbLines, int lineLength)
         }
         line++;
     }
-    return (pos);    
+    return (pos);
 }
 
 char ft_check_next_cell(char **map, t_cell cell)
@@ -227,13 +264,27 @@ char ft_check_next_cell(char **map, t_cell cell)
         c = map[cell.pos_x + 1][cell.pos_y];
     else if (cell.front == 3)
         c = map[cell.pos_x][cell.pos_y - 1];
+    if (cell.front > 3)
+    {
+        ft_putstr("CHECK WARNING!\n");
+        ft_putcell(cell);
+    }
     return (c);
 }
 
-// int ft_is_collectible_found(char **map, t_cell cell, int **alreadyFound)
-// {
-    
-// }
+int ft_is_visited(char cell_content)
+{
+    if (cell_content == '#')
+        return (1);
+    return (0);
+}
+
+int ft_is_wall(char cell_content)
+{
+    if (cell_content == '1')
+        return (1);
+    return (0);
+}
 
 t_cell ft_get_next_cell(t_cell cell)
 {
@@ -245,51 +296,16 @@ t_cell ft_get_next_cell(t_cell cell)
         cell.pos_x += 1;
     else if (cell.front == 3)
         cell.pos_y -= 1;
+    else
+    {
+        ft_putstr("GET WARNING!\n");
+        ft_putcell(cell);
+    }
+    cell.front = 0;
     return (cell);
 }
 
-//function/functions to verify is there is a existing path from P to E passing by all the C can only go on 0 (P and C count as 0) (not specified what happens if player goes on E before collecting every C ? does the game finish or does it do nothing ?)(obviously blocked by 1)
-int ft_check_valid_path(char **map, int nbLines, int lineLength, int countCollectibles)
-{
-    // int i;
-    // int countC;
-    // int alreadyFound[countCollectibles][2];
-    (void) countCollectibles;
-    t_cell cell;
-    t_cell lastCell;
-    t_cell nextCell;
-
-    // i = 0;
-    cell = ft_get_start_pos(map, nbLines, lineLength);
-    //
-    printf("indexes of starting pos [%d][%d]\n", cell.pos_x, cell.pos_y);
-    while (map[cell.pos_x][cell.pos_y] != 'E')
-    {
-        nextCell = ft_get_next_cell(cell);
-        printf("pos [%d][%d]\n", cell.pos_x, cell.pos_y);
-        while (ft_check_next_cell(map, cell) == '1' || (lastCell.pos_x == nextCell.pos_x && lastCell.pos_y == nextCell.pos_y))
-        {
-            cell.front += 1;
-            nextCell = ft_get_next_cell(cell);
-            printf("Turning: %d\n", cell.front);
-            if (cell.front >= 4)
-                return (-1);
-            sleep(1);
-        }
-
-        if (ft_check_next_cell(map, cell) == '0' || ft_check_next_cell(map, cell) == 'C' || ft_check_next_cell(map, cell) == 'E' )
-        {
-                printf("%d\n", cell.front);
-                lastCell = cell;
-                cell = ft_get_next_cell(cell);
-                ft_putstr("Going forward!\n");
-                cell.front = 0;
-        }    
-    }
-    return (0);
-}
-
-void ft_putmap(char **map, int nbLines, int lineLength)
+int ft_is_all_checked(char **map, int nbLines, int lineLength)
 {
     int line;
     int col;
@@ -300,18 +316,69 @@ void ft_putmap(char **map, int nbLines, int lineLength)
         col = 0;
         while (col < lineLength)
         {
-            ft_putchar(map[line][col]);
-            col++;
+            if (ft_is_wall(map[line][col]) || ft_is_visited(map[line][col]))
+                col++;
+            else
+                return (0);
         }
-        ft_putchar('\n');
         line++;
     }
+    return (1);
+}
+
+int ft_check_valid_path(char **map, int nbLines, int lineLength, int collectiblesCount)
+{
+    int countC;
+    int foundE;
+    t_cell startingPos;
+    t_cell cell;
+
+    cell = ft_get_start_pos(map, nbLines, lineLength);
+    startingPos = cell; 
+    countC = 0;
+    foundE = 0;
+    while (1)
+    {
+        if ((ft_is_wall(ft_check_next_cell(map, cell)) == 1) || (ft_is_visited(ft_check_next_cell(map, cell)) == 1))
+        {
+            if (cell.front < 3)
+                cell.front++;
+            else
+            {
+                ft_putstr("Restarting from first position!\n");
+                map[cell.pos_x][cell.pos_y] = '#';
+                cell = startingPos;
+            }
+        }
+        else if (cell.front < 4)
+        {
+            if (ft_check_next_cell(map, cell) == 'E')
+                foundE++;
+            else if (ft_check_next_cell(map, cell) == 'C')
+                countC++;
+            map[cell.pos_x][cell.pos_y] = '#';
+            cell = ft_get_next_cell(cell);
+        }
+        else if (cell.front >= 4)
+        {
+            ft_putstr("======================PB!\n");
+            cell.front = 0;
+        }
+        ft_putcell(cell);
+        ft_putmap(map, nbLines, lineLength);
+        ft_putchar('\n');
+        if (ft_is_all_checked(map, nbLines, lineLength) == 1)
+            break;
+    }
+    if (countC != collectiblesCount || foundE != 1)
+        return (-1);
+    return (0);
 }
 
 void ft_freemap(char **map, int nbLines)
 {
     int line;
-    
+
     line = 0;
     while (line < nbLines)
     {
@@ -327,7 +394,7 @@ char **ft_get_map(char *file, int nbLines, int lineLength)
     int fd;
     int nbBytesRead;
     int line;
-    
+
     nbBytesRead = 0;
     line = 0;
     fd = open(file, O_RDONLY);
@@ -342,7 +409,7 @@ char **ft_get_map(char *file, int nbLines, int lineLength)
         if (!map[line])
             return (close(fd), NULL);
         nbBytesRead = read(fd, map[line], lineLength + 1);
-        if(nbBytesRead == -1)
+        if (nbBytesRead == -1)
             exit(EXIT_FAILURE);
         map[line][lineLength] = 0;
         line++;
@@ -350,12 +417,12 @@ char **ft_get_map(char *file, int nbLines, int lineLength)
     return (close(fd), map);
 }
 
-//test main for the functions above
+// test main for the functions above
 int main(int argc, char **argv)
 {
     int nbLines;
     int lineLength;
-    int countCollectibles;
+    int collectiblesCount;
     char **map;
 
     if (argc < 2)
@@ -386,14 +453,15 @@ int main(int argc, char **argv)
         ft_putstr("La map n'est pas entouree de murs\n");
         return (ft_freemap(map, nbLines), 4);
     }
-    countCollectibles = ft_check_map_requirement(map, nbLines, lineLength);
-    if (countCollectibles == -1)
+    collectiblesCount = ft_check_map_requirement(map, nbLines, lineLength);
+    if (collectiblesCount == -1)
     {
         ft_putstr("Il y a un probleme avec la carte\n");
         return (ft_freemap(map, nbLines), 5);
     }
-    ft_putstr("!!!!!!!!MAP IS GUCCI!!!!!!!!\n");
-    if (ft_check_valid_path(map, nbLines, lineLength, countCollectibles) == -1)
+    else
+        ft_putstr("MAP IS GUD!!!!!!!\n");
+    if (ft_check_valid_path(map, nbLines, lineLength, collectiblesCount) == -1)
     {
         ft_putstr("Bloqueeeee!\n");
     }
--- src/test.c
+++ src/test.c
@@ -22,6 +22,15 @@ t_cell ft_go_to_next_cell(t_cell cell)
 int main(void)
 {
     t_cell cell;
+    char tab[3][2];
+    char tab2[3][2];
+
+    tab[0][0] = '1';
+    tab[0][1] = '1';
+    tab[1][0] = '1';
+    tab[1][1] = '1';
+    tab[2][0] = '1';
+    tab[2][1] = '1';
 
     cell.pos_x = 2;
     cell.pos_y = 2;
@@ -38,4 +47,6 @@ int main(void)
     printf("[%d][%d]\n", cell.pos_x, cell.pos_y);
     cell = ft_go_to_next_cell(cell);
     printf("[%d][%d]\n", cell.pos_x, cell.pos_y);
+    tab2[2][1] = tab[2][1];
+    ft_putchar(tab2[2][1]); 
 }
